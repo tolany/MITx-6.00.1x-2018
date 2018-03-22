a173ade5ea46d344a251e967c26c77e3139554bc
tolany <31434286+tolany@users.noreply.github.com>
2018-03-22 16:30:44 +0900

    Midterm

---
+++ Midterm/Problem5.py
@@ -0,0 +1,23 @@
+# Problem 5
+# 10/10 points (graded)
+# Implement a function that meets the specifications below.
+
+# def deep_reverse(L):
+#     """ assumes L is a list of lists whose elements are ints
+#     Mutates L such that it reverses its elements and also
+#     reverses the order of the int elements in every element of L.
+#     It does not return anything.
+#     """
+
+# For example, if L = [[1, 2], [3, 4], [5, 6, 7]] then deep_reverse(L) mutates L to be [[7, 6, 5], [4, 3], [2, 1]]
+
+#------------------- CODE ----------------#
+
+def deep_reverse(L):
+    result = []
+    for i in L:
+        result.append(i[::-1])
+    L[:] = result[::-1]
+    return L
+#------------------- Result ----------------#
+# Correct
